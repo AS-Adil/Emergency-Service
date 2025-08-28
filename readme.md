5f2d187057c7a90b2381f9e27074049eebf61736
AS-Adil <abdussamadadil1@gmail.com>
2025-08-28 12:37:13 +0600

    added functionalities for call buttons and heart Icon

--- readme.md
+++ readme.md
@@ -76,6 +76,18 @@ This Section will have layout as figma
 
 ---
 
+
+
+
+
+
+
+
+
+
+
+
+
 ## Functionalities
 
 ### 4. Heart Icons
@@ -104,19 +116,8 @@ This Section will have layout as figma
 
 ---
 
-## Create Readme
 
-You have to create a `Readme.md` file. and write down following questions. Dont Try to copy paste from AI Tools. Just write what you know about these. If you don't know , then search , learn , understand and then write.
 
-### 6. Answer the following questions clearly:
-
-1. What is the difference between **getElementById, getElementsByClassName, and querySelector / querySelectorAll**?
-2. How do you **create and insert a new element into the DOM**?
-3. What is **Event Bubbling** and how does it work?
-4. What is **Event Delegation** in JavaScript? Why is it useful?
-5. What is the difference between **preventDefault() and stopPropagation()** methods?
-
----
 
 ## 🧪 Challenges Part (10 Marks)
 
@@ -139,6 +140,40 @@ How to get current local time in js
 ```
 
 ---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Create Readme
+
+You have to create a `Readme.md` file. and write down following questions. Dont Try to copy paste from AI Tools. Just write what you know about these. If you don't know , then search , learn , understand and then write.
+
+### 6. Answer the following questions clearly:
+
+1. What is the difference between **getElementById, getElementsByClassName, and querySelector / querySelectorAll**?
+2. How do you **create and insert a new element into the DOM**?
+3. What is **Event Bubbling** and how does it work?
+4. What is **Event Delegation** in JavaScript? Why is it useful?
+5. What is the difference between **preventDefault() and stopPropagation()** methods?
+
+---
+
+
 
 ## ⚙️ Technology Stack
 
--- script.js
+++ script.js
@@ -1,4 +1,82 @@
+// fuction to re-use 
+function getById (id){
+    const element = document.getElementById(id);
+    return element;
+}
 
 
- let Time = new Date().toLocaleTimeString();
- console.log(Time);
+
+
+// updating hear count 
+document.getElementById('card-container').addEventListener('click', function (e){
+ 
+    if(e.target.className.includes('love')){
+       let currentHeart = Number(getById('heartCount').innerText);
+       let updatedHeart = currentHeart +1;
+       getById('heartCount').innerText= updatedHeart;
+        
+    }
+
+
+})
+
+
+
+
+// functionality for Call buttons 
+document.getElementById('card-container').addEventListener('click', function (e){
+ 
+    if(e.target.className.includes('call-btn')){
+       
+        const currentCoins = Number(getById('coinCount').innerText);
+
+        if(currentCoins < 20){
+            alert(`You Don't Have Enough Coins To Make The Call`)
+            return;
+        }
+
+        // things to be done aftre validation 
+
+        // cutting 20 coins for each call 
+        const updatedCoins = currentCoins - 20;
+        getById('coinCount').innerText = updatedCoins;
+
+        const serviceName = e.target.parentNode.parentNode.children[1].children[0].innerText;
+        // console.log(serviceName);
+
+        const number = e.target.parentNode.parentNode.children[2].children[0].innerText;
+        console.log(number);
+
+        alert(`${serviceName} ${number}`)
+        
+    }
+
+
+})
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // functionality for copy buttons
+// document.getElementById('card-container').addEventListener('click', function (e){
+ 
+//     console.log(e.target.className.includes('copy-btn'));
+//     if(e.target.className.includes('copy-btn')){
+//         console.log('copy btn');
+
+        
+//     }
+
+
+// })
